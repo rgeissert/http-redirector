bf8eac1960102817f73578f589859f38dd2b6032
Raphael Geissert <atomo64@gmail.com>
2012-02-05 13:37:13 -0600

    fix date regex

--- check.pl
+++ check.pl
@@ -279,7 +279,7 @@ sub fetch {
     my ($date, $software) = split /\n/,$trace,3;
 
     return 0
-	unless ($date =~ m/^\w{3} \w{3} \d{2} (?:\d{2}:){2}\d{2} UTC \d{4}$/);
+	unless ($date =~ m/^\w{3} \s+ \w{3} \s+ \d{1,2} \s+ (?:\d{2}:){2}\d{2} \s+ UTC \s+ \d{4}$/x);
 
     $self->{'software'} = $software;
     $self->{'date'} = str2time($date);
