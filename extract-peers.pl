5832d11a2d020d8fb17877d108fdc944b3eac3b6
Raphael Geissert <atomo64@gmail.com>
2012-03-19 20:16:03 -0600

    Script to extract AS peers from zebra-dump-parser's output
    It only extracts those for AS' where mirrors are located.

---
+++ extract-peers.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl -w
+
+####################
+#    Copyright (C) 2012 by Raphael Geissert <geissert@debian.org>
+#
+#    This file is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This file is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this file  If not, see <http://www.gnu.org/licenses/>.
+#
+#    On Debian systems, the complete text of the GNU General
+#    Public License 3 can be found in '/usr/share/common-licenses/GPL-3'.
+####################
+
+use strict;
+use warnings;
+
+use Storable qw(retrieve);
+use Mirror::DB;
+
+use Getopt::Long;
+
+my $mirrors_db_file = 'db';
+my $print_progress = 0;
+my $db_out = 'db.peers';
+
+GetOptions('mirrors-db=s' => \$mirrors_db_file,
+	    'progress!' => \$print_progress,
+	    'store-db=s' => \$db_out);
+
+my $mirrors_db = retrieve($mirrors_db_file);
+
+my %mirror_ASes;
+
+for my $type (keys %{$mirrors_db}) {
+    next if ($type eq 'all');
+
+    for my $AS (keys %{$mirrors_db->{$type}{'AS'}}) {
+	$AS =~ s/^AS//;
+	$mirror_ASes{$AS} = 1;
+    }
+}
+
+my %as_routes;
+my $count = -1;
+
+$count = 0 if ($print_progress);
+
+while (<>) {
+    my @parts = split;
+    next unless (scalar(@parts) >= 3);
+
+    my @dests = pop @parts;
+    my $peer = pop @parts;
+
+    if ($dests[0] =~ s/^\{// && $dests[0] =~ s/\}$//) {
+	@dests = split (/,/, $dests[0]);
+    }
+
+    my $distance = 1;
+    for my $dest (@dests) {
+	next unless (exists($mirror_ASes{$dest}));
+
+	$as_routes{$peer} = {}
+	    unless (exists($as_routes{$peer}));
+	$as_routes{$peer}->{$dest} = $distance;
+    }
+
+    if ($count != -1 && ($count++)%1000 == 0) {
+	print STDERR "Processed: $count...\r";
+    }
+}
+
+Mirror::DB::set($db_out);
+Mirror::DB::store(\%as_routes);
