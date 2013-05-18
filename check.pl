0f1117e6ff76bde863f1c20533ca9697754716a6
Raphael Geissert <atomo64@gmail.com>
2013-05-18 14:22:17 +0200

    Print the original stamp and the one we are going to use

--- check.pl
+++ check.pl
@@ -180,7 +180,7 @@ for my $type (keys %traces) {
 		if (defined($global_master_stamp) &&
 		    (($global_master_stamp - $stamp) > 12*3600 ||
 		     $type eq 'security' || $is_type_ref)) {
-		    print "Overriding the master stamp of $type/$continent\n";
+		    print "Overriding the master stamp of $type/$continent (from $stamp to $global_master_stamp)\n";
 		    $master_stamps{$continent} = $global_master_stamp;
 		} elsif (!defined($global_master_stamp)) {
 		    $global_master_stamp = $stamp;
