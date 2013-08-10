54be4056b83264cc72f718f1ed2fc6f9e640ea00
Raphael Geissert <atomo64@gmail.com>
2013-08-10 13:49:25 +0200

    Module for creating fake Geoip::Record entries

---
+++ Mirror/Fake/Geoip/Record.pm
@@ -0,0 +1,51 @@
+package Mirror::Fake::Geoip::Record;
+
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    my $self = {};
+
+    my %data;
+    {
+	my $even = 1;
+	my $key = '';
+
+	foreach my $elem (@_) {
+	    if ($even) {
+		$even = 0;
+		$key = $elem;
+	    } else {
+		$even = 1;
+		$data{$key} = $elem;
+	    }
+	}
+    }
+    $self->{'data'} = \%data;
+
+    bless ($self, $class);
+    return $self;
+}
+
+sub latitude {
+    my $self = shift;
+    return $self->{'data'}->{'latitude'};
+}
+
+sub longitude {
+    my $self = shift;
+    return $self->{'data'}->{'longitude'};
+}
+
+sub country_code {
+    my $self = shift;
+    return $self->{'data'}->{'country_code'};
+}
+
+sub continent_code {
+    my $self = shift;
+    return $self->{'data'}->{'continent_code'};
+}
+
+1;
